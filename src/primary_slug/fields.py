f9e54e5ee74be09b95540766e9c15d869c265038
George Notaras <gnot@g-loaded.eu>
2011-06-04 18:38:12 +0300

    Several improvements of the PrimarySlugField.

--- src/primary_slug/fields.py
+++ src/primary_slug/fields.py
@@ -28,26 +28,54 @@ from django.db.models.fields import SlugField
 from django.core.urlresolvers import get_callable
 from primary_slug import settings
 from primary_slug import utils
+from primary_slug.forms import PrimarySlugFormField
 
 
 default_slugify = get_callable(settings.PRIMARY_SLUG_SLUGIFY_FUNC)
 
 class PrimarySlugField(SlugField):
-    """
-    Slugfield
+    """This model field is an enhanced version of Django's ``SlugField``.
+    
+    *django-primary-slug* provides an enhanced version of the default Django's
+    ``SlugField``, which supports:
+    
+    - Custom slugify function.
+    - Custom list of valid characters.
+    
+    The ``PrimarySlugField`` derives from the default ``SlugField`` and thus
+    all attributes and methods of the default ``SlugField`` are inherited.
+    
+    In addition to the default arguments, the ``PrimarySlugField`` also
+    supports the following:
+    
+    ``populate_from``
+        A string or callable that returns the initial value to be slugified.
+        If this is not set, then the slug field is not populated automatically.
+        Also, if the slug field is set by the user, then it is not auto-
+        populated.
+    ``slugify``
+        A callable python object which slugifies the value retrieved from the 
+        ``populate_from`` field. If this is not set, then the function defined
+        in the ``PRIMARY_SLUG_SLUGIFY_FUNC`` setting is used.
+
     """
     
     def __init__(self, *args, **kwargs):
-        super(PrimarySlugField, self).__init__(*args, **kwargs)
         
         self.populate_from = kwargs.pop('populate_from', None)
-        
+        if self.populate_from:
+            # Here we force blank=True or else the form validation will fail.
+            kwargs['blank'] = True
+            
         self.slugify = kwargs.pop('slugify', default_slugify)
         assert hasattr(self.slugify, '__call__')
+        
+        super(PrimarySlugField, self).__init__(*args, **kwargs)
+        
     
     def pre_save(self, instance, add):
         """Returns field's value just before saving."""
-        
+
         # get currently entered slug
         slug = self.value_from_object(instance)
 
@@ -60,10 +88,19 @@ class PrimarySlugField(SlugField):
             slug = self.slugify(populate_from_field_value)
     
             if slug:
-                # Crop slug
-                slug = utils.crop_slug(self, slug)
-                # Make the slug available as instance attribute
+                # Hard reset slug length
+                if self.max_length < len(slug):
+                    slug = slug[:self.max_length]
+                
+                assert slug, 'slug is not set'
+                
+                # Make the slug available as instance attribute as if the user had set it
                 setattr(instance, self.name, slug)
         
         return slug
+    
+    def formfield(self, **kwargs):
+        defaults = {'form_class': PrimarySlugFormField}
+        defaults.update(kwargs)
+        return super(PrimarySlugField, self).formfield(**defaults)
 
